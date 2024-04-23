81db92f6d465f89a7e8962cbaad51d89ced62b59
JOAO ROSIVALDO GOMES <rosivaldo.joao@academico.ifpb.edu.br>
2024-04-22 21:52:34 -0300

    Finalizando

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,4 +1,59 @@
 public class ContaTerminal {
 
+    private int numeroConta;
+    private String numeroAgencia;
+    private String nomeCLiente;
+    private float saldo;
+
+    public ContaTerminal() {
+    }
+
+    @Override
+    public String toString() {
+        return "Olá "+ getNomeCLiente()+", obrigado por criar uma conta em nosso banco, sua agencia é "+ getNumeroAgencia() +
+                ", conta "+ getNumeroConta() + " e seu saldo de "+ getSaldo() + " ja esta disponível para saque.";
+    }
+
+    public ContaTerminal(int numeroConta, String numeroAgencia, String nomeCLiente, float saldo) {
+        this.numeroConta = numeroConta;
+        this.numeroAgencia = numeroAgencia;
+        this.nomeCLiente = nomeCLiente;
+        this.saldo = saldo;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public String getNumeroAgencia() {
+        return numeroAgencia;
+    }
+
+    public void setNumeroAgencia(String numeroAgencia) {
+        this.numeroAgencia = numeroAgencia;
+    }
+
+    public String getNomeCLiente() {
+        return nomeCLiente;
+    }
+
+    public void setNomeCLiente(String nomeCLiente) {
+        nomeCLiente = nomeCLiente;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+
+
 
 }
--- ContaBanco/src/Main.java
+++ ContaBanco/src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner leitor = new Scanner(System.in);
+        String nomeCliente,agencia;
+        int numeroConta;
+        float saldo;
+
+        System.out.println("Por favor digite seu nome: ");
+        nomeCliente = leitor.nextLine();
+        System.out.println("Por favor digite o número da agência: ");
+        agencia = leitor.nextLine();
+        System.out.println("Agora digite o numero de sua conta: ");
+        numeroConta = Integer.parseInt(leitor.nextLine());
+        System.out.println("Digite agora o valor que será credito como saldo de abertura da conta: ");
+        saldo = Float.parseFloat(leitor.nextLine());
+
+        ContaTerminal contaTerminal = new ContaTerminal(numeroConta,agencia,nomeCliente,saldo);
+
+        System.out.println(contaTerminal.toString());
+
+
     }
 }
